022931a09a697dd70b3ea4fed89a9f8ed48bdd19
Fordjour Gyasi Samuel <sgfordjour@st.ug.edu.gh>
2024-06-26 08:56:33 +0000

    initial commit

---
+++ CardsScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CardsScreen = () => {
+  return (
+    <View>
+      <Text>CardsScreen</Text>
+    </View>
+  )
+}
+
+export default CardsScreen
+
+const styles = StyleSheet.create({})
